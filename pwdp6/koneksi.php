dcea603441c79122408d792bc9b73f25db38a7a4
SHANYSASNITAANDRIANI <68839802+SHANYSASNITAANDRIANI@users.noreply.github.com>
2021-11-25 20:50:14 +0700

    1900018176_SHANY SASNITA ANDRIANI

---
+++ pwdp6/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host="localhost";
+$username="root";
+$password="";
+$con=@mysqli_connect($host,$username,$password,$databasename);
+if (!$con) {
+ echo "Error: " . mysqli_connect_error();
+exit();
+}
+?>
